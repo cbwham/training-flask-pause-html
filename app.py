7cea3c658448ecb22fbbf505a434193a55febc59
Kurt Link <42419314+LINK3N80@users.noreply.github.com>
2024-03-14 14:40:01 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, jsonify
+from datetime import datetime
+import time
+
+app = Flask(__name__)
+
+# Funktion zur Abrufung der aktuellen Uhrzeit
+def get_current_time():
+    return datetime.now().strftime('%H:%M:%S')
+
+# Route für die Startseite mit fortlaufender Uhrzeit-Anzeige
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route für die aktuelle Uhrzeit im JSON-Format
+@app.route('/get_time')
+def get_time():
+    return jsonify({'time': get_current_time()})
+
+# Route für die Pausenzeiten
+@app.route('/zeiten')
+def zeiten():
+    current_time = datetime.now()
+    montag_freitag_pausen = "09:30-09:45"
+    montag_donnerstag_pausen = "11:15-11:30, 12:15-13:15"
+
+    if 9 <= current_time.hour < 10 and 30 <= current_time.minute < 46 and current_time.weekday() in range(0, 5):
+        return f"Aktuelle Pause {montag_freitag_pausen} am {current_time.strftime('%A')}. Pausenzeiten: Montags-Freitags: {montag_freitag_pausen}, Montags-Donnerstags: {montag_donnerstag_pausen}."
+
+    if 11 <= current_time.hour < 12 and 15 <= current_time.minute < 31 and current_time.weekday() in range(0, 4):
+        return f"Aktuelle Pause {montag_donnerstag_pausen.split(',')[0]} am {current_time.strftime('%A')}. Pausenzeiten: Montags-Freitags: {montag_freitag_pausen}, Montags-Donnerstags: {montag_donnerstag_pausen}."
+
+    if 12 <= current_time.hour < 14 and 15 <= current_time.minute <= 0 and current_time.weekday() in range(0, 4):
+        return f"Aktuelle Pause {montag_donnerstag_pausen.split(',')[1]} am {current_time.strftime('%A')}. Pausenzeiten: Montags-Freitags: {montag_freitag_pausen}, Montags-Donnerstags: {montag_donnerstag_pausen}."
+
+    return f"Im Augenblick keine Pause. Pausenzeiten: Montags-Freitags: {montag_freitag_pausen}, Montags-Donnerstags: {montag_donnerstag_pausen}."
+
+if __name__ == '__main__':
+    app.run(debug=True)
